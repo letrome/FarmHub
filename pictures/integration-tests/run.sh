8f23cc73c802efec41f180d3866b8a1b88d7e0b7
Romain Letourneur <14938066+letrome@users.noreply.github.com>
2024-05-15 10:23:38 +0200

    add exec, up workflow and add logs in run.sh

--- pictures/integration-tests/run.sh
+++ pictures/integration-tests/run.sh
@@ -2,8 +2,10 @@
 
 cd "$(dirname "$0")" || exit
 cd ..
+echo $PWD
 
 go run main.go localhost:5500&
+echo $?
 
 sleep 1
 newman run integration-tests/newman-tests.json -e integration-tests/envs.json

f5f046e2c8b7226babaeba61eb7dc1975655c6de
Romain Letourneur <14938066+letrome@users.noreply.github.com>
2024-05-15 13:34:26 +0200

    up

--- pictures/integration-tests/run.sh
+++ pictures/integration-tests/run.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")" || exit
 cd ..
 
 go run . localhost:5500&
-
 sleep 1
+
 newman run integration-tests/newman-tests.json -e integration-tests/envs.json
 kill "$(lsof -t -i :5500)"

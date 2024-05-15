43c9b6b0bab89d927c64cca6e5cc9c3cd5d81966
Romain Letourneur <14938066+letrome@users.noreply.github.com>
2024-05-15 09:29:46 +0200

    explicit main.go in intg tests run

--- pictures/integration-tests/run.sh
+++ pictures/integration-tests/run.sh
@@ -3,7 +3,8 @@
 cd "$(dirname "$0")" || exit
 cd ..
 
-go run . localhost:5500&
+go run main.go localhost:5500&
+
 sleep 1
 newman run integration-tests/newman-tests.json -e integration-tests/envs.json
 kill "$(lsof -t -i :5500)"

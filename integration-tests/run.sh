edebea73b2c720e84c946f8eef303bbcb7c34702
Romain Letourneur <14938066+letrome@users.noreply.github.com>
2024-06-05 10:24:35 +0200

    add sleep in intg tests

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -5,40 +5,44 @@ cd "$(dirname "$0")/.." || exit
 # Launch the stack
 ## Launch species
 cd species || exit
-echo "SPECIES: $PWD"
 node server.js 3300&
+sleep 5
+echo "species service launched"
 cd .. || exit
 
 ## Launch animals
 cd animals || exit
-echo "ANIMALS: $PWD"
 php -S 127.0.0.1:4400 -t public&
+sleep 5
+echo "animals service launched"
 cd .. || exit
 
 ## Launch pictures
 cd pictures || exit
-echo "PICTURES: $PWD"
 go run . 127.0.0.1:5500&
+sleep 5
+echo "pictures service launched"
 cd .. || exit
 
 ## Launch farmers
 cd farmers || exit
-echo "FARMERS: $PWD"
 python main.py --port=6600&
+sleep 5
+echo "farmers service launched"
 cd .. || exit
 
 ## Launch farm
 cd farm || exit
-echo "FARM: $PWD"
 export SPRING_PROFILES_ACTIVE=intg
 mvn spring-boot:run&
+sleep 10
+echo "farm service launched"
 cd .. || exit
 
 sleep 5
 
 # Launch the integration tests
 cd integration-tests || exit
-echo "INTG-TESTS: $PWD"
 newman run newman-tests.json -e envs.json
 
 # Teardown
